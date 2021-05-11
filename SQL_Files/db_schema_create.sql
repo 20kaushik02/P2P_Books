0ed21a69db7a9e457660f836f8a0b52dcec454e1
Kaushik <rknarayan02@gmail.com>
2021-05-11 10:01:43 +0530

    Modifications to schema creation file and typo correction

--- SQL_Files/db_schema_create.sql
+++ SQL_Files/db_schema_create.sql
@@ -2,5 +2,4 @@ DROP DATABASE IF EXISTS p2p_books;
 CREATE DATABASE p2p_books;
 \c p2p_books;
 CREATE SCHEMA IF NOT EXISTS p2p_books_schema;
-ALTER DATABASE p2pbooks SET search_path TO p2p_books_schema, public;
-ALTER ROLE postgres SET search_path TO p2p_books_schema, public;
+ALTER DATABASE p2p_books SET search_path TO p2p_books_schema, public;
