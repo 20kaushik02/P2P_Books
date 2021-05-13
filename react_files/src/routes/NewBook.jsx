e745ddf56229b1cefa744415129e68e4794288d7
Kaushik <rknarayan02@gmail.com>
2021-05-13 23:30:30 +0530

    Front end route for adding new book into database

---
+++ react_files/src/routes/NewBook.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddBook from '../components/AddBook'
+import Header from '../components/Header'
+
+const NewBook = () => {
+    return (
+        <div>
+           <Header/>
+           <AddBook/>
+        </div>
+    )
+}
+
+export default NewBook;
