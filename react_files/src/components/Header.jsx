b5e6912c808786385112c63337f0b55afce73691
Kaushik <rknarayan02@gmail.com>
2021-05-14 17:45:28 +0530

    Header links to home page, added front end auth API if anyone wants to try front end authentication

--- react_files/src/components/Header.jsx
+++ react_files/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <div>
-            <h1 className = "font-weight-light display-1 text-center">P2P Books</h1>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <h1 className = "font-weight-light display-1 text-center">P2P Books</h1>
+            </Link>
         </div>
     )
 }
