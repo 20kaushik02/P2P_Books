8e022d55bf2ab63d2f4094567b7f3859d51e49ab
Kaushik <rknarayan02@gmail.com>
2021-05-14 13:33:33 +0530

    Added react route to search page

--- react_files/src/App.jsx
+++ react_files/src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { BooksContextProvider } from './context/BooksContext';
+import { CategoriesContextProvider } from './context/CategoriesContext';
 import Home from './routes/Home';
 import NewBook from './routes/NewBook';
+import SearchResults from './routes/SearchResults';
 
 const App = () => {
     return (
         <BooksContextProvider>
-            <div className="container">
-                <Router>
-                    <Switch>
-                        <Route exact path = "/" component={Home}/>
-                        <Route exact path = "/newbook" component={NewBook}/>
-                    </Switch>
-                </Router>
-            </div>
+            <CategoriesContextProvider>
+                <div className="container">
+                    <Router>
+                        <Switch>
+                            <Route exact path = "/" component={Home}/>
+                            <Route exact path = "/newbook" component={NewBook}/>
+                            <Route exact path = "/search" component={SearchResults}/>
+                        </Switch>
+                    </Router>
+                </div>
+            </CategoriesContextProvider>
         </BooksContextProvider>
                 
     );
