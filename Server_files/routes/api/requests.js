be9308a09af319912f1b6002270d16d8062b4fa4
George <george@Kaarmac4s-MacBook-Air.local>
2021-05-17 11:21:38 +0530

    me dumb

---
+++ Server_files/routes/api/requests.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../DB_files");
+const tokenCheck = require("../../middleware/tokenCheck");
+
+//SUP NK
--- Server_files/routes/api/transactions.js
+++ Server_files/routes/api/transactions.js
@@ -2,3 +2,5 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../DB_files");
 const tokenCheck = require("../../middleware/tokenCheck");
+
+//accept an offer/make a transaction
