3796882a403558449cd1834f74cfe9ff880149e6
George <george@Kaarmac4s-MacBook-Air.local>
2021-05-22 08:42:32 +0530

    small changes

--- Server_files/routes/api/transactions.js
+++ Server_files/routes/api/transactions.js
@@ -4,11 +4,23 @@ const db = require("../../DB_files");
 const tokenCheck = require("../../middleware/tokenCheck");
 
 //accept an offer/make a transaction
-router.post("/insert", tokenCheck, (req,res) => {
+router.post("/insert", tokenCheck, async (req,res) => {
     try {
-        const username = req.user;
-        const get_result = await db.query("INSERT INTO transactions(date_o")  
+        const {offer_id, return_date} = req.body;
+        let date_ob = new Date();
+        let curr_date = date_ob.toISOString().split('T')[0]
+        const get_result = await db.query("INSERT INTO transactions(date_of_transac, return_date, offer_id)\
+        values($1,$2,$3) RETURNING *", [curr_date, return_date, offer_id]);
+        console.log(get_result.rows);
+        res.status(201).json({
+        status: "success",
+        data: {
+        transaction_details: get_result.rows[0],
+       },
+    });
     }catch (err){
       console.log(err);
     }
 });
+
+module.exports = router;
