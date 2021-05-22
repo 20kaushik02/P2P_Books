47a832d96d5ee19278fa9b0622aba78441f16619
George <george@Kaarmac4s-MacBook-Air.local>
2021-05-22 09:55:38 +0530

    small change

--- Server_files/routes/api/transactions.js
+++ Server_files/routes/api/transactions.js
@@ -23,4 +23,40 @@ router.post("/insert", tokenCheck, async (req,res) => {
     }
 });
 
+//get user transactions
+router.get("/get", tokenCheck, async (req,res) => {
+  try {
+      const username = req.user;
+      const get_result = await db.query("SELECT t.transaction_id, t.date_of_transac, t.return_date, o.renter FROM \
+      transactions t INNER JOIN offers o ON t.offer_id = o.offer_id INNER JOIN books_active ba ON\
+      ba.book_active_id = o.book_active_id AND ba.owner = $1", [username]);
+      console.log(get_result.rows);
+      res.status(201).json({
+      status: "success",
+      data:{
+        transaction_details: get_result.rows,
+       },
+    });
+  }catch(err){
+    console.log(err);
+  }
+});
+
+//update transaction(i.e. return date of transaction)
+router.put("/change", tokenCheck, async (req,res) => {
+try {
+    const {transaction_id, return_date} = req.body;
+    const get_result = await db.query("UPDATE transactions SET return_date = $1 WHERE transaction_id = $2 RETURNING *",
+    [return_date, transaction_id]);
+    console.log(get_result.rows);
+      res.status(201).json({
+      status: "success",
+      data:{
+        new_transaction_details: get_result.rows,
+       },
+    });
+} catch(err){
+  console.log(err);
+}
+});
 module.exports = router;
