72b55ea891f82aafba0c2a6ab67c66abf4fd9794
George <george@Kaarmac4s-MacBook-Air.local>
2021-05-23 13:58:44 +0530

    sorry streets

--- SQL_Files/view_books_active_loc_create.sql
+++ SQL_Files/view_books_active_loc_create.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW books_active_loc AS (
-    SELECT ba.*,b.title,b.author,u.location_id,l.state,l.city,l.area FROM books_active ba 
+    SELECT ba.*,b.title,b.author,u.location_id,l.state,l.city,l.area,l.street FROM books_active ba 
     INNER JOIN books b ON ba.books_id = b.books_id 
     INNER JOIN users u on u.username = ba.owner 
     INNER JOIN location l ON l.location_id = u.location_id
