15cbfa9c47634bb7ec53fb7f92625fc44f2dbff1
R Nanda Kumar <jerrynandak@gmail.com>
2021-05-25 14:41:01 +0530

    Notification triggers

--- SQL_Files/trig_create.sql
+++ SQL_Files/trig_create.sql
@@ -2,6 +2,8 @@
 
 DROP TRIGGER IF EXISTS TransactOfferCleanupTrig_ai ON transactions;
 DROP TRIGGER IF EXISTS UserReputationUpdateTrig_ai ON transactions;
+DROP TRIGGER IF EXISTS RequestFulfilMessageTrig_ai ON books_active;
+DROP TRIGGER IF EXISTS OwnerOfferMessageTrig_ai ON offers;
 
 CREATE TRIGGER TransactOfferCleanupTrig_ai
 AFTER INSERT ON transactions
@@ -12,3 +14,13 @@ CREATE TRIGGER UserReputationUpdateTrig_ai
 AFTER INSERT ON transactions
 FOR EACH ROW
 EXECUTE PROCEDURE UserReputationUpdateProc();
+
+CREATE TRIGGER RequestFulfilMessageTrig_ai
+AFTER INSERT OR UPDATE ON books_active
+FOR EACH ROW
+EXECUTE PROCEDURE RequestFulfilMessage();
+
+CREATE TRIGGER OwnerOfferMessageTrig_ai
+AFTER INSERT ON offers
+FOR EACH ROW
+EXECUTE PROCEDURE OwnerOfferMessage();
--- SQL_Files/trig_func_create.sql
+++ SQL_Files/trig_func_create.sql
@@ -18,22 +18,63 @@ CREATE OR REPLACE FUNCTION TransactOfferCleanupProc()
         DECLARE final_renter users.username%TYPE;
         DECLARE temp_doi offers.offer_id%TYPE;
         DECLARE user_name offers.renter%TYPE;
+        DECLARE b_title books.title%TYPE;
     BEGIN
         SELECT book_active_id, renter INTO final_bai, final_renter FROM offers 
             WHERE offer_id = NEW.offer_id;
-    
+
+        SELECT b.title INTO b_title FROM books b INNER JOIN books_active ba ON 
+            ba.books_id = b.books_id AND ba.book_active_id = final_bai;
+
         FOR temp_doi IN (SELECT offer_id FROM offers 
             WHERE book_active_id = final_bai AND renter != final_renter) 
         LOOP
             SELECT renter INTO user_name FROM offers
                 WHERE offer_id = temp_doi;
 
-            INSERT INTO notification(username,message) VALUES(user_name, 'your offer has been rejected');
+            INSERT INTO notification(username,message) VALUES(user_name, 'Your offer for "' ||b_title|| '" was rejected');
             DELETE FROM offers WHERE offer_id = temp_doi;
         END LOOP;
         RETURN NULL;
     END;
     $TransactOfferCleanupTrig_ai$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION RequestFulfilMessage()
+    RETURNS TRIGGER AS $RequestFulfilMessageTrig_ai$
+    
+        DECLARE requester_name requests.requester%TYPE;
+        DECLARE temp_bid books.books_id%TYPE;
+        DECLARE req_book books.title%TYPE;
+    BEGIN
+        
+        FOR temp_bid, requester_name IN (SELECT books_id, requester FROM requests) LOOP
+            IF (temp_bid = NEW.books_id) AND (NEW.book_status = 'A') THEN
+                SELECT b.title INTO req_book FROM books b INNER JOIN books_active ba 
+                ON b.books_id = ba.books_id AND ba.books_id = temp_bid;
+                
+                INSERT INTO notification(username,message) 
+                VALUES(requester_name, 'Your request for "' ||req_book|| '" has been fulfilled. Head to the home page to make an offer');
+            END IF;
+        END LOOP;
+        RETURN NULL;
+    END;
+    $RequestFulfilMessageTrig_ai$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION OwnerOfferMessage()
+    RETURNS TRIGGER AS $OwnerOfferMessageTrig_ai$
+    
+        DECLARE owner_name books_active.owner%TYPE;
+        DECLARE owner_book books.title%TYPE;
+    BEGIN
+        SELECT ba.owner, b.title INTO owner_name, owner_book FROM offers o INNER JOIN books_active ba
+        ON o.book_active_id = ba.book_active_id AND o.book_active_id = NEW.book_active_id
+        INNER JOIN books b ON ba.books_id = b.books_id;
+
+        INSERT INTO notification(username,message) 
+        VALUES(owner_name, 'New offer for your book,"' ||owner_book|| '"');
+        RETURN NULL;
+    END;
+    $OwnerOfferMessageTrig_ai$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION UserReputationUpdateProc()
     RETURNS TRIGGER
